604d4d560d3a5306496e5c6a3d05681698e750c1
borson <mdnsborson@gmail.com>
2024-08-23 08:57:36 +0600

    js added

--- js/area-calculator.js
+++ js/area-calculator.js
@@ -5,11 +5,18 @@ document.getElementById("triangle-btn").addEventListener("click",function getTri
     
     const hFild = document.getElementById("triangle-h");
     const hValue = parseFloat(hFild.value);
-    
-    const area = 0.5 * bValue * hValue;
+
+    if(isNaN(bValue) || isNaN(hValue)){
+        alert("Please Entir Number");
+        return;
+    }else{
+        const area = 0.5 * bValue * hValue;
     
     const areaValue = document.getElementById("triangleArea");
     areaValue.innerText = area;
+    }
+    
+    
 })
 
 // Rectangle calculator
@@ -26,3 +33,60 @@ document.getElementById("rectangle-btn").addEventListener("click", function getR
     areaValue.innerText = area;
 })
 
+// parallelogram calclutor
+
+document.getElementById("parallelogram-btn").addEventListener("click", function getParallelogramArea(){
+    const bFild = document.getElementById("parallelogram-b");
+    const bValue =parseFloat(bFild.value);
+    
+    const hFild = document.getElementById("parallelogram-h");
+    const hValue = parseFloat(hFild.value);
+    
+    const area = bValue * hValue;
+    
+    const areaValue = document.getElementById("parallelogramArea");
+    areaValue.innerText = area;
+})
+
+// rhombus calclutor
+
+document.getElementById("rhombus-btn").addEventListener("click", function getRhombusArea(){
+    const d1Fild = document.getElementById("rhombus-d1");
+    const d1Value =parseFloat(d1Fild.value);
+    
+    const d2Fild = document.getElementById("rhombus-d2");
+    const d2Value = parseFloat(d2Fild.value);
+    
+    const area = 0.5 * d1Value * d2Value;
+    
+    const areaValue = document.getElementById("rhombusArea");
+    areaValue.innerText = area;
+})
+
+// Pentagon calclutor
+document.getElementById("Pentagon-btn").addEventListener("click", function getPentagonArea(){
+    const pFild = document.getElementById("pentagon-p");
+    const pValue =parseFloat(pFild.value);
+    
+    const bFild = document.getElementById("pentagon-b");
+    const bValue = parseFloat(bFild.value);
+    
+    const area = 0.5 * pValue * bValue;
+    
+    const areaValue = document.getElementById("pentagonArea");
+    areaValue.innerText = area;
+})
+
+// ellipse calclutor
+document.getElementById("ellipse-btn").addEventListener("click", function getEllipseArea(){
+    const aFild = document.getElementById("ellipse-a");
+    const aValue =parseFloat(aFild.value);
+    
+    const bFild = document.getElementById("ellipse-b");
+    const bValue = parseFloat(bFild.value);
+    
+    const area = 3.1416 * aValue * bValue;
+    
+    const areaValue = document.getElementById("ellipseArea");
+    areaValue.innerText = area;
+})
